65cbbafddb91cef7b0afdf97543c80f7329621f4
Andre Schalkau <andre@catchafish.dev>
2022-09-16 22:04:55 +0200

    Init Commit

---
+++ nuxt/components/pages/index/HeroArea.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <v-parallax
+    height="400"
+    src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+  >
+    <v-container>header</v-container>
+  </v-parallax>
+</template>
+
+<style scoped></style>
